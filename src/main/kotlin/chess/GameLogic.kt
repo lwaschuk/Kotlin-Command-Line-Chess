d9418af9e4663de0c71e26d0758d050a41062311
Lukas Waschuk <lwaschuk@ualberta.ca>
2023-01-26 18:09:58 -0700

    Split GameLogic from the ChessBoard

---
+++ src/main/kotlin/chess/GameLogic.kt
@@ -0,0 +1,90 @@
+package chess
+
+import pieces.Color
+import pieces.Pawn
+import pieces.PieceType
+
+class GameLogic(chessBoard: ChessBoard) {
+    private var chessBoard: ChessBoard
+
+    init {
+        this.chessBoard = chessBoard
+    }
+
+    fun startGame() {
+        initPawns()
+        chessBoard.render()
+    }
+
+    private fun initPawns() {
+        for (col in 0 until 8) {
+            chessBoard.setPiece(Location(1, col), Pawn(Color.W))
+            chessBoard.setPiece(Location(6, col), Pawn(Color.B))
+        }
+    }
+
+    fun gameOver(turn: Turn): Boolean {
+        return reachEnd() || elimAll() || stalemate(turn)
+    }
+
+    private fun elimAll(): Boolean {
+        var whiteFound = false
+        var blackFound = false
+
+        for (row in 0 until 8) {
+            for (col in 0 until 8) {
+                when (chessBoard.getPiece(Location(row, col)).color) {
+                    Color.B -> blackFound = true
+                    Color.W -> whiteFound = true
+                    else -> {}
+                }
+            }
+        }
+        if (whiteFound && !blackFound) {
+            println("\nWhite Wins!")
+            return true
+        }
+        if (blackFound && !whiteFound) {
+            println("\nBlack Wins!")
+            return true
+        }
+        return false
+    }
+
+    private fun reachEnd(): Boolean {
+        for (col in 0 until 8) {
+            if (chessBoard.getPiece(Location(0, col)).color == Color.B) {
+                println("\nBlack Wins!")
+                return true
+            }
+            if (chessBoard.getPiece(Location(7, col)).color == Color.W) {
+                println("\nWhite Wins!")
+                return true
+            }
+        }
+        return false
+    }
+
+    private fun stalemate(turn: Turn): Boolean {
+        for (row in 0 until 7) {
+            for (col in 0 until 8) {
+                val piece = chessBoard.getPiece(Location(row, col))
+                if (piece.color == turn.getColor() && piece.canMove(Location(row, col), this.chessBoard, turn)
+                        .isNotEmpty()
+                ) {
+                    return false
+                }
+            }
+        }
+        println("\nStalemate!")
+        return true
+    }
+
+    fun sourceCoordinateVerifier(chessMove: ChessMove, turn: Turn): Boolean {
+        val startCoordinates = chessMove.startLocation()
+        val piece = chessBoard.getPiece(startCoordinates)
+        return piece.type == PieceType.PAWN && piece.color == turn.getColor()
+    }
+
+
+}
