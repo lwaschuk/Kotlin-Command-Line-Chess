ced4607ff0c1bb677c6ca1dbfdb777d56a9feaae
Lukas Waschuk <77379787+Napkinzz@users.noreply.github.com>
2023-02-04 06:38:16 -0700

    Update GameLogic.kt
    Added logic for:
    * Check
    * Checkmate
    * Stalemate
    
    will need to refactor before merge

--- src/main/kotlin/chess/GameLogic.kt
+++ src/main/kotlin/chess/GameLogic.kt
@@ -11,53 +11,159 @@ class GameLogic(chessBoard: ChessBoard) {
 
     fun startGame() {
         initChessPieces()
+//        test()
         chessBoard.render()
     }
 
-    fun gameOver(turn: Turn): Boolean {
-        return kingsEliminated() || stalemate(turn)
+    private fun test() {
+        chessBoard.setPiece(Location(5,6), Rook(Color.W))
+        chessBoard.setPiece(Location(6,3), Queen(Color.W))
+        chessBoard.setPiece(Location(0,2), King(Color.W))
+        chessBoard.setPiece(Location(7,6), King(Color.B))
     }
 
-    private fun kingsEliminated(): Boolean {
-        var whiteFound = false
-        var blackFound = false
-
-        for (row in ChessBoard.ROW_START..ChessBoard.ROW_END) {
-            for (col in ChessBoard.COL_START..ChessBoard.COL_END) {
-                val currentPiece = chessBoard.getPiece(Location(row, col))
-                if (currentPiece is King) {
-                    when (currentPiece.color) {
-                        Color.B -> blackFound = true
-                        Color.W -> whiteFound = true
-                        else -> {}
-                    }
-                }
-            }
-        }
-        if (whiteFound && !blackFound) {
-            println("\nWhite Wins!")
-            return true
-        }
-        if (blackFound && !whiteFound) {
-            println("\nBlack Wins!")
-            return true
-        }
-        return false
-    }
-
-    private fun stalemate(turn: Turn): Boolean {
-        for (row in ChessBoard.ROW_START .. ChessBoard.ROW_END) {
-            for (col in ChessBoard.COL_START .. ChessBoard.COL_END) {
-                val location = Location(row, col)
-                val piece = chessBoard.getPiece(location)
-                // only check pc's for the current turn (black or white)
-                if (piece.color == turn.getColor() && piece.canMove(location, this.chessBoard, turn).isNotEmpty()) {
+    /*
+    Finds every piece that the player has then finds every move of every piece and temporarily moves it to its
+    end location to run check() and if no moves exist that can take the players king out of check the game ends
+    */
+    fun legalMoves(turn: Turn): Boolean {
+        val pieceLocations = allLocations(turn)
+        for (location in pieceLocations) {
+            val allMoves = findMoves(location, chessBoard, turn)
+            for (move in allMoves) {
+                val inCheck = tmpMove(move, chessBoard, turn)
+                if (!inCheck) {
                     return false
                 }
             }
         }
-        println("\nStalemate!")
         return true
+    }
+
+    fun check(turn: Turn): Boolean {
+        val enemyMoves = mutableSetOf<ChessMove>()
+        val enemyTurn = Turn(!turn.getTurn()) // FIX THIS LATER
+
+        val kingLocation = kingsLocation(turn)
+
+        // king los like a queen
+        enemyMoves.addAll(losAsQueen(kingLocation, turn, enemyTurn))
+        // king los like a knight
+        enemyMoves.addAll(losAsKnight(kingLocation, turn, enemyTurn))
+
+        // if king location in enemy moves return true
+        for (move in enemyMoves) {
+            if (kingLocation == move.endLocation()) {
+                return true
+            }
+        }
+        return false
+    }
+
+    private fun losAsQueen(kingLocation: Location, turn: Turn, enemyTurn: Turn): Set<ChessMove> {
+        val enemyMoves = mutableSetOf<ChessMove>()
+        // check diagonals and rows/col as if the king was a queen
+        val queenDirections = listOf(
+            Location(1, 1), Location(1, -1),
+            Location(-1, 1), Location(-1, -1),
+            Location(1, 0), Location(-1, 0),
+            Location(0, 1),Location(0, -1)
+        )
+        // if the king sees a pc of opposite color, get the possibleMoves
+        for (direction in queenDirections) {
+            var nextLocation = kingLocation + direction
+            while (nextLocation.isValid(nextLocation)) {
+                val nextPiece = chessBoard.getPiece(nextLocation)
+                if (nextPiece.color == turn.getColor()) {
+                    break
+                }
+                else if (nextPiece.color == turn.enemyColor()) {
+                    enemyMoves.addAll(findMoves(nextLocation, chessBoard, enemyTurn))
+                    break
+                }
+                else if (nextPiece.color == Color.E) {
+                    nextLocation += direction
+                }
+            }
+        }
+        return enemyMoves
+    }
+
+    private fun losAsKnight (kingLocation: Location, turn: Turn, enemyTurn: Turn): Set<ChessMove> {
+        val enemyMoves = mutableSetOf<ChessMove>()
+        // check locations as if the king was a knight
+        val knightDirections = listOf(
+            Location(2, -1), Location(2, 1),
+            Location(-2, -1), Location(-2, 1),
+            Location(1, 2), Location(-1, 2),
+            Location(1, -2), Location(-1, -2)
+        )
+        // if the king sees a pc of opposite color, get the possibleMoves
+        for (direction in knightDirections) {
+            val nextLocation = kingLocation + direction
+            if (nextLocation.isValid(nextLocation)) {
+                val nextPiece = chessBoard.getPiece(nextLocation)
+                if (nextPiece.color == turn.enemyColor()) {
+                    enemyMoves.addAll(findMoves(nextLocation, chessBoard, enemyTurn))
+                }
+            }
+        }
+        return enemyMoves
+    }
+
+    private fun findMoves(location: Location, chessBoard: ChessBoard, turn: Turn): Set<ChessMove> {
+        val possibleMoves = mutableSetOf<Location>()
+        val piece = chessBoard.getPiece(location)
+        possibleMoves.addAll(piece.canMove(location, chessBoard, turn))
+
+        val chessMoves = mutableSetOf<ChessMove>()
+        for (end in possibleMoves) {
+            chessMoves.add(ChessMove(location, end))
+        }
+        return chessMoves
+    }
+
+    private fun kingsLocation(turn: Turn): Location {
+        for (row in ChessBoard.ROW_START..ChessBoard.ROW_END) {
+            for (col in ChessBoard.COL_START..ChessBoard.COL_END) {
+                val currentPiece = chessBoard.getPiece(Location(row, col))
+                if ((currentPiece is King) && currentPiece.color == turn.getColor()) {
+                    return Location(row, col)
+                }
+            }
+        }
+        return Location(0,0)
+    }
+
+    private fun tmpMove(chessMove: ChessMove, chessBoard: ChessBoard, turn: Turn): Boolean {
+        val startLocation = chessMove.startLocation()
+        val endLocation = chessMove.endLocation()
+        val startPiece = chessBoard.getPiece(startLocation)
+        val tmpPiece = chessBoard.getPiece(endLocation)
+
+        chessBoard.setPiece(endLocation, startPiece)
+        chessBoard.setPiece(startLocation, EmptySpot())
+
+        val inCheck = check(turn)
+
+        chessBoard.setPiece(startLocation, startPiece)
+        chessBoard.setPiece(endLocation, tmpPiece)
+
+        return inCheck
+    }
+
+    private fun allLocations(turn: Turn): Set<Location> {
+        val possibleMoves = mutableSetOf<Location>()
+        for (row in ChessBoard.ROW_START..ChessBoard.ROW_END) {
+            for (col in ChessBoard.COL_START..ChessBoard.COL_END) {
+                val currentPiece = chessBoard.getPiece(Location(row, col))
+                if ((currentPiece !is EmptySpot) && currentPiece.color == turn.getColor()) {
+                    val piece = Location(row, col)
+                    possibleMoves.add(piece)
+                }
+            }
+        }
+        return possibleMoves
     }
 
     fun sourceCoordinateVerifier(chessMove: ChessMove, turn: Turn): Boolean {
@@ -67,7 +173,7 @@ class GameLogic(chessBoard: ChessBoard) {
     }
 
     private fun initChessPieces() {
-//        initPawns()
+        initPawns()
         initKnights()
         initRooks()
         initBishops()
