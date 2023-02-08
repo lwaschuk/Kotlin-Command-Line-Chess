6bd8e8813b0c01345b0c914bd076629a9fa1defb
Lukas Waschuk <77379787+Napkinzz@users.noreply.github.com>
2023-02-07 17:03:40 -0700

    ChessBoard Documentation

--- src/main/kotlin/game_helpers/ChessBoard.kt
+++ src/main/kotlin/game_helpers/ChessBoard.kt
@@ -2,9 +2,20 @@ package game_helpers
 
 import pieces.*
 
+/**
+ * A chessboard class that contains public methods to get/set a piece on the board, as well as render the board
+ *
+ * @param nothing
+ */
 class ChessBoard {
     private var chessBoard = Array(8) { Array<IChessPiece>(8) { EmptySpot() } }
 
+    /**
+     * Print the board to the console
+     *
+     * @param nothing
+     * @return nothing
+     */
     fun render() {
         println("  +---+---+---+---+---+---+---+---+")
         for (row in ROW_END downTo ROW_START) {
@@ -22,10 +33,22 @@ class ChessBoard {
         println("    a   b   c   d   e   f   g   h")
     }
 
+    /**
+     * Gets a chess piece that was set on the board
+     *
+     * @param coordinates The location of the piece we are interested in
+     * @return The piece
+     */
     fun getPiece(coordinates: Location): IChessPiece {
         return this.chessBoard[coordinates.row()][coordinates.column()]
     }
 
+    /**
+     * Sets a chess piece on the board
+     *
+     * @param coordinates The location of the piece we are interested in setting
+     * @return nothing
+     */
     fun setPiece(location: Location, piece: IChessPiece) {
         this.chessBoard[location.row()][location.column()] = piece
     }
