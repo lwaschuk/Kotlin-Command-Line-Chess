a800f765c0d9d906aabe3d0e1330f9bb64506463
Lukas Waschuk <77379787+Napkinzz@users.noreply.github.com>
2023-02-07 17:10:06 -0700

    Documentation for ChessMove

--- src/main/kotlin/game_helpers/ChessMove.kt
+++ src/main/kotlin/game_helpers/ChessMove.kt
@@ -1,8 +1,18 @@
 package game_helpers
 
+/**
+ * A class that contains the Start Location and the End Location
+ *
+ * @param nothing
+ */
 class ChessMove {
     private val chessMove: Pair<Location, Location>
 
+    /**
+     * Constructs the chessMove from the user input ex. a2a4
+     *
+     * @param input the string the user input
+     */
     constructor(input: String) {
         val startRow = input[1] - '1'
         val startCol = input[0] - 'a'
@@ -15,19 +25,43 @@ class ChessMove {
         this.chessMove = Pair(startCoordinates, endCoordinates)
     }
 
+    /**
+     * Constructs the chessMove from a Start Location and End Location
+     *
+     * @param start The starting Location
+     * @param end The ending Location
+     */
     constructor(start: Location, end: Location) {
         this.chessMove = Pair(start, end)
     }
 
+    /**
+     * Method to return the starting location of a chess move
+     *
+     * @return the starting location
+     */
     fun startLocation(): Location {
         return this.chessMove.first
     }
 
+    /**
+     * Method to return the ending location of a chess move
+     *
+     * @return the ending location
+     */
     fun endLocation(): Location {
         return this.chessMove.second
     }
 
     companion object {
+        /**
+         * A method to convert a set of chess moves from how it is stored ((0,1),(0,3))
+         * to a human-readable representation ((a,2),(a,4))
+         *
+         * @param input the set of chess moves to convert
+         *
+         * @return the set of moves in a human-readable representation
+         */
         fun convertSetOfLocations(input: Set<Location>): Set<Pair<Char, Char>> {
             val newSet = mutableSetOf<Pair<Char, Char>>()
             for (location in input) {
